e3bb902836cc6339c40348871427c948893bacc3
Joydip Koley <joydipkoley12@gmail.com>
2025-06-24 23:46:26 +0530

    perfect squre problem solving code

---
+++ perfect_squres.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int numSquares(int n) {
+    int* dp = (int*)malloc((n + 1) * sizeof(int));
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        dp[i] = INT_MAX;
+        for (int j = 1; j * j <= i; j++) {
+            if (dp[i - j * j] + 1 < dp[i]) {
+                dp[i] = dp[i - j * j] + 1;
+            }
+        }
+    }
+
+    int result = dp[n];
+    free(dp);
+    return result;
+}
+int main() {
+    int n = 13;
+    int result = numSquares(n);
+    printf("Least number of perfect squares for %d is %d\n", n, result);
+    return 0;
+}
